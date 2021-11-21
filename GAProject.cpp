ed6197763932fab8eaa5f0e5fa7ff30804c9f59e
Puthchy <nariththanomsup@gmail.com>
2021-11-21 23:44:40 +0700

    randfinish

--- GAProject.cpp
+++ GAProject.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <time.h>
 using namespace std;
+
 struct data{
     int value,weight;
 };
 
-void read_file()
+void read_file(int *itemN)
 {
     struct data Data[5000];
     int sum = 0;
@@ -18,34 +19,40 @@ void read_file()
         cerr << "Unable to open file datafile.txt";
         exit(1);   // call system to stop
     }
-    int itemN,MaxW;
-    inFile >> itemN >> MaxW;
+    int MaxW;
+    inFile >> *itemN >> MaxW;
     int i = 0;
     while (inFile) {
         inFile >> Data[i].value >> Data[i].weight;
         i++;
     }
-    for(i = 0; i < itemN; i++){
+    for(i = 0; i < *itemN; i++){
         cout << Data[i].value << " " << Data[i].weight << "\n";
     }
     inFile.close();
 }
 
-void random_choice()
+void random_choice(int *itemN)
 {
     int randombi;
-        srand (time(NULL));
-    for(int i = 0; i < 10; i++){
-        cout << rand() % 2;
+    for (int j = 0; j < 10; j++)
+    {
+        cout << j+1 << ".) ";
+        srand (j+time(NULL));
+        for(int i = 0; i < *itemN; i++){
+            cout << rand() % 2;
+        }
+        cout << "\n";
     }
-    cout << "\n";
-    
-   
 }
 
-int main(){   
-    read_file();
-    random_choice();
-    random_choice();
+
+
+int main(){  
+    int itemN;
+    read_file(&itemN);
+    int item[10][itemN];
+    random_choice(&itemN);
+    cout << itemN;
     return 0;
 }
