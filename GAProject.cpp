f5a83da15e46962e2c7dc34448848d40ce5990dd
Puthchy <nariththanomsup@gmail.com>
2021-11-21 22:07:14 +0700

    read

--- GAProject.cpp
+++ GAProject.cpp
@@ -5,12 +5,27 @@ struct data{
     int value,weight;
 };
 
-void read_file
+void read_file()
 {
+    int sum = 0;
+    int x;
+    ifstream inFile;
+    inFile.open("Example Set.txt");
 
+    if (!inFile) {
+    cerr << "Unable to open file datafile.txt";
+    exit(1);   // call system to stop
+    }
+
+    while (inFile >> x) {
+        sum = sum + x;
+    }
+    inFile.close();
+    cout << "Sum = " << sum << endl; 
 }
 
 int main(){   
     cout << "Hello World";
+    read_file();
     return 0;
 }
