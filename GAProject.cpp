b3cc087d9410678c86b0aca44edff2e4c7ee69ed
fifa254144 <48977414+fifa254144@users.noreply.github.com>
2021-12-02 21:50:20 +0700

    Rankingสุดน่ารัก

--- GAProject.cpp
+++ GAProject.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
+#include <algorithm>
 using namespace std;
 int pop = 10,itemN,MaxW;
 struct data{
-    int value = 0,weight = 0;
+    int value = 0,weight = 0,index = -1;
 }Data[5000],Score[10];
 
 void read_file(int *itemN)
@@ -62,6 +63,7 @@ void FittingTest(int *item,int n){
             Score[n].value += Data[i].value;
             Score[n].weight += Data[i].weight;
         }
+        Score[n].index = n;
     }
     if(Score[n].weight > MaxW)
         Score[n].value = 0;
@@ -74,6 +76,15 @@ void FittingTest(int *item,int n){
 //     }
 // }
 
+bool compareTwoPop(data a, data b){
+    if (a.value >= b.value)
+        return a.value > b.value;
+}
+
+void RankingSelection(data Score[]){
+    sort(Score, Score + pop , compareTwoPop());
+}
+
 int main(){  
     read_file(&itemN);
     int item[pop][itemN];
@@ -82,15 +93,24 @@ int main(){
     cout << "\n---------- Default Data ----------\n";
     for(int timess = 0 ; timess < pop ; timess ++)
         printData(item[timess]);
-    for(int timess = 0 ; timess < pop ; timess += 2)
-        uniformCross(item[timess]);
+    cout << "\n---------- Fitting Test ----------\n";  
+    for(int timess = 0 ; timess < pop ; timess ++)
+        FittingTest(item[timess],timess);
+    for(int timess = 0 ; timess < pop ; timess ++)
+        cout << timess+1 << ") Value : " << Score[timess].value << " Weight : " << Score[timess].weight << "\n";
+    
+    cout << "\n---------- Ranking Selection ----------\n";
+    RankingSelection(Score);
+
     cout << "\n---------- Uniform Cross ----------\n";
+    for(int timess = 0 ; timess < pop ; timess += 2) // Uniform
+        uniformCross(item[timess]);
     for(int timess = 0 ; timess < pop ; timess ++)
         printData(item[timess]);
     cout << "\n---------- Fitting Test ----------\n";    
     for(int timess = 0 ; timess < pop ; timess ++)
         FittingTest(item[timess],timess);
     for(int timess = 0 ; timess < pop ; timess ++)
-        cout << timess+1 << ") Value : " <<Score[timess].value << " Weight : " << Score[timess].weight << "\n";
+        cout << timess+1 << ") Value : " << Score[timess].value << " Weight : " << Score[timess].weight << "\n";
     return 0;
 }
